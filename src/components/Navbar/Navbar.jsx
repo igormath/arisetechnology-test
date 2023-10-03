0a8f4123eb01cacf414ac492896603a18fb6b1d1
igormath <igorxt9@gmail.com>
2023-10-03 16:31:08 -0300

    Update Navbar component with tailwind classes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,12 +2,21 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/search/name">Busca por nome</Link>
-            <Link to="/search/firstletter">Busca por primeira letra</Link>
-            <Link to="/search/ingredients">Busca por ingredientes</Link>
-            <Link to="/bookmarks">Receitas favoritas</Link>
+        <nav className="w-full h-full">
+            <ul className="w-full h-full flex flex-row justify-evenly items-center py-2">
+                <li className="w-2/12 h-full text-center text-primary bg-button-color hover:opacity-80 rounded md:text-2xl">
+                    <Link to="/">Home</Link>
+                </li>
+                <li className="w-2/12 text-primary bg-button-color hover:opacity-80 rounded md:text-2xl text-center h-full">
+                    <Link to="/search/name">Busca por nome</Link>
+                </li>
+                <li className="w-2/12 text-primary bg-button-color hover:opacity-80 rounded md:text-2xl text-center h-full">
+                    <Link to="/search/firstletter">Busca por primeira letra</Link>
+                </li>
+                <li className="w-2/12 text-primary bg-button-color hover:opacity-80 rounded md:text-2xl text-center h-full">
+                    <Link to="/search/ingredients">Busca por ingredientes</Link>
+                </li>
+            </ul>
         </nav>
     );
 };
