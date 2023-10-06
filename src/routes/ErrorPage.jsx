0827c0fb2084cb5e9a5b76067b306fb89da09116
igormath <igorxt9@gmail.com>
2023-10-05 21:18:04 -0300

    Update error page UI

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
     return (
-        <div>
-            <h1>404 error!</h1>
-        </div>
+        <main className="bg-white-card py-4 px-4">
+            <h1 className="text-4xl text-center">404: Página não encontrada!</h1>
+            <Link className="text-2xl block mx-auto my-10 bg-button-color text-button rounded text-center hover:opacity-80 w-2/3 md:w-2/12">Voltar para Home</Link>
+            
+        </main>
     );
 };
 
