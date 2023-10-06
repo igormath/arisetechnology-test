1f475d53485473e6b3ddef48351fd11deb4ce49c
igormath <igorxt9@gmail.com>
2023-10-05 22:16:34 -0300

    Add footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer className="mx-auto px-4 sm:px-6 lg:px-8 bg-primary w-full h-15 p-5">
+            <p className="text-center text-xl">Desenvolvido por <a className="text-link" href="https://github.com/igormath">Igor Azevedo</a></p>
+            <p className="text-center text-xl">2023 ©</p>
+        </footer>
+    );
+};
+
+export default Footer;
