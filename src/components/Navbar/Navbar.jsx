259c00e5ae931fadb9273fba871e285466c21b34
igormath <igorxt9@gmail.com>
2023-10-05 21:27:18 -0300

    Update Navbar w/ sandwich menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,51 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import {FaBars, FaTimes} from "react-icons/fa"
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleMenu = () => {
+        setIsOpen((prev) => !prev);
+    };
+
     return (
-        <nav className="w-full h-full">
-            <ul className="w-full h-full flex flex-row justify-evenly items-center py-2">
-                <li className="w-2/12 h-full text-center text-primary bg-button-color hover:opacity-80 rounded md:text-2xl">
-                    <Link to="/">Home</Link>
-                </li>
-                <li className="w-2/12 text-primary bg-button-color hover:opacity-80 rounded md:text-2xl text-center h-full">
-                    <Link to="/search/name">Busca por nome</Link>
-                </li>
-                <li className="w-2/12 text-primary bg-button-color hover:opacity-80 rounded md:text-2xl text-center h-full">
-                    <Link to="/search/firstletter">Busca por primeira letra</Link>
-                </li>
-                <li className="w-2/12 text-primary bg-button-color hover:opacity-80 rounded md:text-2xl text-center h-full">
-                    <Link to="/search/ingredients">Busca por ingredientes</Link>
-                </li>
-            </ul>
-        </nav>
+        <header className="bg-primary">
+            <div className="mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex items-center justify-between h-20">
+                    <div className="flex items-center">
+                        <h1 className="text-title text-4xl font-bold">Arise Receitas</h1>
+                    </div>
+                    {/* Navlinks */}
+                    <nav className="hidden md:block">
+                        <ul className="flex items-center justify-evenly">
+                            <li className="text-center"><Link to="/" className="text-primary transition-all duration-500 hover:bg-secondary hover:text-subtitle px-3 py-2 rounded-md text-md font-medium">Início</Link></li>
+                            <li className="text-center"><Link to="/search/name" className="text-primary transition-all duration-500 hover:bg-secondary hover:text-subtitle px-3 py-2 rounded-md text-md font-medium">Pesquisar Receitas</Link></li>
+                            <li className="text-center"><Link to="/search/firstletter" className="text-primary transition-all duration-500 hover:bg-secondary hover:text-subtitle px-3 py-2 rounded-md text-md font-medium">Receitas por Letra</Link></li>
+                            <li className="text-center"><Link to="/search/ingredients" className="text-primary transition-all duration-500 hover:bg-secondary hover:text-subtitle px-3 py-2 rounded-md text-md font-medium text-center">Receitas por Ingredientes</Link></li>
+                        </ul>
+                    </nav>
+                    {/* hamburguer button */}
+                    <div className="-mr-2 flex md:hidden">
+                        <button type="button" onClick={handleMenu} className="inline-flex items-center justify-center p-2 rounded-md text-title hover:text-button hover:bg-button-color">
+                            <span className="sr-only">Open main menu</span>
+                            {isOpen === true ? <FaTimes/> : <FaBars/>}
+                        </button>
+                    </div>
+                </div>
+            </div>
+            {/* mobile-menu */}
+            {isOpen ? (
+                <nav className="md:hidden">
+                    <ul className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                        <li className="text-center"><Link to="/" className="text-primary hover:bg-secondary hover:text-subtitle block px-3 py-2 rounded-md text-base font-medium">Início</Link></li>
+                        <li className="text-center"><Link to="/search/name" className="text-primary hover:bg-secondary hover:text-subtitle block px-3 py-2 rounded-md text-base font-medium">Pesquisar Receitas</Link></li>
+                        <li className="text-center"><Link to="/search/firstletter" className="text-primary hover:bg-secondary hover:text-subtitle block px-3 py-2 rounded-md text-base font-medium">Receitas por Letra</Link></li>
+                        <li className="text-center"><Link to="/search/ingredients" className="text-primary hover:bg-secondary hover:text-subtitle block px-3 py-2 rounded-md text-base font-medium">Receitas por Ingredientes</Link></li>
+                    </ul>
+                </nav>
+            ): null}
+        </header>
     );
 };
 
